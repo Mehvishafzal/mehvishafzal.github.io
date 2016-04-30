fbf297c5bb49db6d6ca3b47b2870f6ceeaec21bb
Mehvishafzal <mehvish_afzal@harvard.edu>
2016-04-30 10:32:27 -0400

    Store the text code

--- main.js
+++
@@ -1,6 +0,0 @@
-var retain = document.getElementById('button1');
-retain.style.backgroundColor = "red"
-retain.addEventListner("click", function (){
-  document.getElementById('button1') = "happy3.png";
-};
-
